9727a5d68e91da10f240a9ded96854c391899d69
Chandan Tiwari <chandantiwari11@gmail.com>
2025-03-17 13:54:15 +0530

    First Commit

---
+++ src/test/java/com/example/simple_springboot_crud/SimpleSpringbootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simple_springboot_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringbootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
